1d769aea1d19387b9296e698c9a6fc67b589d0fd
michael.armitage <michael.armitage@derivitec.com>
2024-01-25 12:19:15 +0000

    Initial commit

---
+++ test/cdk-arm.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkArm from '../lib/cdk-arm-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-arm-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkArm.CdkArmStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
