63c41010cfe164df89d00b12de03d1a7550686f9
michael.armitage <michael.armitage@derivitec.com>
2024-01-25 16:17:38 +0000

    arm codebuild default

--- lib/build-stack.ts
+++ lib/build-stack.ts
@@ -7,16 +7,16 @@ export class BuildStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
 
-      var x86Image = LinuxBuildImage.fromAsset(this, "x86Image",
-      {
-          directory: "./assets/build/amazon2023_x86"
-      });
-
-
       var armImage = LinuxBuildImage.fromAsset(this, "ArmImage",
       {
           directory: "./assets/build/amazon2023_arm",
           platform: Platform.LINUX_ARM64
+      });
+
+      var x86Image = LinuxBuildImage.fromAsset(this, "x86Image",
+      {
+          directory: "./assets/build/amazon2023_x86",
+          platform: Platform.LINUX_AMD64
       });
     }
 }
--- lib/cdk-arm-stack.ts
+++ lib/cdk-arm-stack.ts
@@ -3,6 +3,7 @@ import { GitHubTrigger } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import BuildStage from './build-stage';
+import { LinuxArmBuildImage } from 'aws-cdk-lib/aws-codebuild';
 
 export class CdkArmStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,7 +11,7 @@ export class CdkArmStack extends cdk.Stack {
 
     const pipeline = new CodePipeline(this, "ArmPipeline", {
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("m17kea/cdk-arm", "main", {
+        input: CodePipelineSource.gitHub("m17kea/cdk-arm", "arm", {
           trigger: GitHubTrigger.WEBHOOK,
           authentication: cdk.SecretValue.secretsManager("cdk-arm")
         }),
@@ -22,7 +23,12 @@ export class CdkArmStack extends cdk.Stack {
         }),
         publishAssetsInParallel: false,
         crossAccountKeys:true,
-        dockerEnabledForSynth: true
+        dockerEnabledForSynth: true,
+        codeBuildDefaults: {
+          buildEnvironment: {
+            buildImage: LinuxArmBuildImage.AMAZON_LINUX_2_STANDARD_3_0,
+          }
+        }
       }
     );
 
